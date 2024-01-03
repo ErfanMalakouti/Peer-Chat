3b37a6f5f445c6e55da6c4fef2b2fd11714ec1b7
malakouti <malakouti@mahsan.co>
2024-01-03 13:07:24 +0330

    create peer 2 peer chat app

--- main.js
+++ main.js
@@ -3,10 +3,24 @@ let token = null;
 let uid = String(Math.floor(Math.random() * 10000));
 let client;
 let channel;
+let queryString = window.location.search;
+let urlParams = new URLSearchParams(queryString);
+let roomID = urlParams.get("room");
 
-let localeStream;
+if (!roomID) {
+  window.location = "lobby.html";
+}
+
+let localStream;
 let remoteStream;
 let peerConnection;
+let constraints = {
+  video: {
+    width: { min: 640, ideal: 1920, max: 1920 },
+    height: { min: 480, ideal: 1080, max: 1080 },
+  },
+  audio: true,
+};
 
 //google free stun servers
 const servers = {
@@ -21,17 +35,21 @@ let init = async () => {
   client = await AgorRtm.createInstance(APP_ID);
   await client.login({ uid, token });
 
-  channel = client.createChannel("roomID");
+  channel = client.createChannel(roomID);
   await channel.join();
 
   channel.on("MemberJoined", handelUserJoined);
+  channel.on("MemberLeft", handelUserLeft);
+
   channel.on("MemberJoined", handelMessageFromPeer);
 
-  localeStream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
-  });
-  document.getElementById("user-1").srcObject = localeStream;
+  localStream = await navigator.mediaDevices.getUserMedia(constraints);
+  document.getElementById("user-1").srcObject = localStream;
+};
+
+let handelUserLeft = (MemberId) => {
+  document.getElementById("user-2").style.display = "none";
+  document.getElementById("user-1").classList.remove("small-frame");
 };
 
 //listener for messages ,offers ,answers and candidates
@@ -60,17 +78,19 @@ let createPeerConnection = async (MemberId) => {
   peerConnection = new RTCPeerConnection(servers);
   remoteStream = new MediaStream();
   document.getElementById("user-2").srcObject = remoteStream;
+  document.getElementById("user-2").style.display = "block";
+  document.getElementById("user-1").classList.add("small-frame");
 
-  if (!localeStream) {
-    localeStream = await navigator.mediaDevices.getUserMedia({
+  if (!localStream) {
+    localStream = await navigator.mediaDevices.getUserMedia({
       audio: true,
       video: true,
     });
-    document.getElementById("user-1").srcObject = localeStream;
+    document.getElementById("user-1").srcObject = localStream;
   }
 
-  localeStream.getTracks().forEach((track) => {
-    peerConnection.addTrack(track, localeStream);
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
   });
 
   peerConnection.ontrack = (e) => {
@@ -115,10 +135,49 @@ let createAnswer = async (MemberId, offer) => {
   });
 };
 
-let addAnswer = async (awnser) => {
+let addAnswer = async (answer) => {
   if (!peerConnection.currentRemoteDescription) {
     peerConnection.currentRemoteDescription(answer);
   }
 };
+
+let leaveChannel = async () => {
+  await channel.leave();
+  await client.logout();
+};
+
+let toggleCamera = async () => {
+  let videoTrack = localStream
+    .getTracks()
+    .find((track) => track.kind === "video");
+  if (videoTrack.enabled) {
+    videoTrack.enabled = false;
+    document.getElementById("mute-cam").style.display = "block";
+    document.getElementById("unmute-cam").style.display = "none";
+  } else {
+    videoTrack.enabled = true;
+    document.getElementById("unmute-cam").style.display = "block";
+    document.getElementById("mute-cam").style.display = "none";
+  }
+};
+
+let toggleMic = async () => {
+  let audioTrack = localStream
+    .getTracks()
+    .find((track) => track.kind === "audio");
+  if (audioTrack.enabled) {
+    audioTrack.enabled = false;
+    document.getElementById("mute-mic").style.display = "block";
+    document.getElementById("unmute-mic").style.display = "none";
+  } else {
+    audioTrack.enabled = true;
+    document.getElementById("unmute-mic").style.display = "block";
+    document.getElementById("mute-mic").style.display = "none";
+  }
+};
+
+document.getElementById("camera-btn").addEventListener("click", toggleCamera);
+document.getElementById("mic-btn").addEventListener("click", toggleMic);
+window.addEventListener("beforeunload", leaveChannel);
 
 init();
